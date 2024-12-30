8073c55663bed70d901c44a641717c9cc69c39a4
bharadwaj-t4 <bharadwajtheruvath14@gmail.com>
2024-12-30 20:44:06 +0530

    Changes made in DFS,

--- ADS/DFS.c
+++ ADS/DFS.c
@@ -1,77 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int adj[20][20], stack[20], top = -1, n, i, j, start, visited[20];
+void dfs(int node);
 
-void dfs(int start);
+int visited[20],i,adj[20][20],j,n,start;
+int vertex[20];
 
-int main()
-{
-    printf("\nEnter number of nodes: ");
-    scanf("%d", &n);
-
-    // Initialize visited array to 0
-    for (i = 0; i < n; i++) {
-        visited[i] = 0;
+int main(){
+    printf("Enter the number of nodes :");
+    scanf("%d",&n);
+    for (i=0; i<n; i++) {
+        printf("Enter the vertices: ");
+        scanf("%d", &vertex[i]);
     }
-
-    // Reading graph from adjacency matrix
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("\nA[%d][%d]: ", i, j);
-            scanf("%d", &adj[i][j]);
+    int flag, weight;
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++)
+        {
+            adj[i][j] = 0;
+            adj[j][i] = 0;
+            printf("Do you want edges between vertice %d & %d \n1. Yes 2. No \n", vertex[i], vertex[j]);
+            scanf("%d", &flag);
+            if (flag==1) {
+                printf("Enter the weight/cost: ");
+                scanf("%d", &weight);
+                adj[i][j] = weight;
+                adj[j][i] = weight;
+            }
         }
     }
 
-    printf("\nEnter starting Node: ");
-    scanf("%d", &start);
 
-    // Validate starting node
-    if (start < 0 || start >= n) {
-        printf("Invalid starting node!\n");
-        return 1;
+    printf("The adjacency matrix: \n");
+    for(i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
     }
+    printf("\n");
 
-    printf("\nDFS Visited Order:\n");
-    dfs(start);
 
+    for(i=0;i<n;i++)
+    {
+        visited[i]=0;
+    }
+    printf("Enter the node to start BFS with :");
+    scanf("%d",&start);
+    for (i=0; i<n; i++) {
+        if (start == vertex[i]) {
+            dfs(i);
+            break;
+        }
+    }
     return 0;
 }
 
 void dfs(int node)
 {
-    // Visiting the node
+    int j;
     visited[node] = 1;
-    printf("%d ", node);
-
-    for (i = 0; i < n; i++) {
-        if (adj[node][i] == 1 && visited[i] != 1) {
-            dfs(i);
+    printf("%d\t",vertex[node]);
+    for(j=0;j<n;j++)
+    {
+        if(adj[node][j] == 1 && visited[j] == 0)
+        {
+            dfs(j);
         }
     }
 }
-
-/*-------Using Stack for DFS--------*/
-/*
-void dfs_iterative(int start)
-{
-    top = -1; // Initialize stack top
-    stack[++top] = start; // Push starting node onto stack
-
-    while (top != -1) { // While stack is not empty
-        int node = stack[top--]; // Pop a node
-
-        if (!visited[node]) {
-            visited[node] = 1; // Mark the node as visited
-            printf("%d ", node);
-
-            // Push all unvisited neighbors onto the stack
-            for (i = n - 1; i >= 0; i--) {
-                if (adj[node][i] == 1 && !visited[i]) {
-                    stack[++top] = i;
-                }
-            }
-        }
-    }
-}
-*/
